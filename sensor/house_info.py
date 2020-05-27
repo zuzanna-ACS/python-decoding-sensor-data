9406654c17a65ec2d747f50f18119745d23c3382
Hugo Valle <hugovalle1@weber.edu>
2020-05-27 14:58:55 -0600

    remove extra lines

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -18,5 +18,4 @@ class HouseInfo(object):
         for record in self.data:
             if rec_date.strftime("%m/%d/%y") == record['date']: 
                 field_data.append(record[field])
-
         return field_data
