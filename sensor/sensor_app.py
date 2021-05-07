390f2ab74be45850b8dc43e4c9a4848ce6879a96
zuzanna-ACS <zuzanna-ACS@github.com>
2021-05-07 12:12:00 +0200

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -2,6 +2,9 @@
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import date,datetime
+from humidity_info import HumidityData
+from statistics import mean
+from particle_count_info import ParticleData
 
 
 ##############################
@@ -25,6 +28,26 @@ recs = house_info.get_data_by_date("id",rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
+recs = humidity_data.get_data_by_date(rec_date=test_date)
+print("House Humidity sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tAverrage: {} humidity".format(mean(recs)))
+particle_data = ParticleData(data)
+recs = particle_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+recs = particle_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Particle sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
 # Module 4 code here:
 
