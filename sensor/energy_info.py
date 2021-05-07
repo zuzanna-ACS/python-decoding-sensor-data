6deaaf1e8d8685e7a90f1896fa6c6bcf685f4685
zuzanna-ACS <zuzanna-ACS@github.com>
2021-05-07 13:45:18 +0200

    Completed module

---
+++ sensor/energy_info.py
@@ -0,0 +1,32 @@
+from house_info import HouseInfo
+from datetime import date,datetime
+
+class EnergyData(HouseInfo):
+    ENERGY_PER_BULB = 0.2
+    ENERGY_BITS = 0x0F0
+
+    def _get_energy(self,rec):
+        energy = int(rec,base=16)
+        energy = energy & self.ENERGY_BITS 
+        energy = energy >>4
+        return energy
+
+    def _convert_data(self,data):
+        recs = []
+        for rec in data: 
+            recs.append(self._get_energy(rec))
+        return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("energy_usage", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("energy_usage", rec_date)
+        return self._convert_data(recs)
+
+    def calculate_energy_usage(self,data):
+        total_energy = sum([field * self.ENERGY_PER_BULB for field in data])
+        return total_energy
+
+
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,6 +6,7 @@ from humidity_info import HumidityData
 from statistics import mean
 from particle_count_info import ParticleData
 from temperature_info import TemperatureData
+from energy_info import EnergyData
 
 
 ##############################
@@ -65,4 +66,14 @@ concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
 # Module 5 code here:
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
