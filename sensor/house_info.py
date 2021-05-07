5e3e1797fe8d9107db930307c6e994eafda66e5e
zuzanna-ACS <zuzanna-ACS@github.com>
2021-05-07 12:47:37 +0200

    Completed module

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -16,7 +16,7 @@ class HouseInfo:
     def get_data_by_date(self,field,rec_date = date.today()):
         field_data = []
         for record in self.data: 
-            if record['date'] == rec_date.strftime('%m/%d/%y'): 
+            if rec_date.strftime('%m/%d/%y') == record['date']: 
                 field_data.append(record[field])
         return field_data
 
